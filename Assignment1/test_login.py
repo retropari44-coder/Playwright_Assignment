b7b74832798d65b0adfd5e086e2572809fda57fa
retropari44-coder <retropari44@gmail.com>
2026-09-22 01:23:24 +0530

    Initial commit: Playwright Pytest framework setup

---
+++ Assignment1/test_login.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page,expect
+import re
+
+def test_loginpage(page:Page):
+    page.goto('https://eventhub.rahulshettyacademy.com')
+    expect(page.get_by_placeholder("you@email.com")).to_be_visible()
+    expect(page.get_by_role('button', name='Sign In')).to_be_visible()
+    expect(page.get_by_role('heading', name='Sign in to EventHub')).to_be_visible()
+
+def test_passwordpage(page:Page):
+    page.goto('https://eventhub.rahulshettyacademy.com')
+    expect(page.locator('#password')).to_be_visible()
+    expect(page).to_have_url(re.compile(r'/login'))  #expect(page).to_have_url('url')
+    expect(page.get_by_role('heading', name='Sign in to EventHub')).to_be_visible()
