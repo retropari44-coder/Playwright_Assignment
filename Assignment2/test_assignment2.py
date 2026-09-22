032bb02e113c8f54743535f5d8e7815aa1b2052f
retropari44-coder <retropari44@gmail.com>
2026-09-22 17:03:55 +0530

    Assignment Updated

---
+++ Assignment2/test_assignment2.py
@@ -0,0 +1,38 @@
+import re
+import pytest
+from playwright.sync_api import Playwright, expect, Page
+
+@pytest.mark.parametrize("browserInstance", ["chromium", "firefox"])
+def test_firstTest(playwright: Playwright, browserInstance):
+    if browserInstance == "chromium":
+        browser = playwright.chromium.launch()
+    elif browserInstance == "firefox":
+        browser = playwright.firefox.launch()
+    context = browser.new_context(base_url='https://eventhub.rahulshettyacademy.com')
+    page = context.new_page()    
+    page.goto('/login')    
+    expect(page).to_have_title(re.compile(r'EventHub'))
+    expect(page.get_by_placeholder('you@email.com')).to_be_visible()
+    expect(page.get_by_role('button', name='Sign In')).to_be_visible()    
+    context.close()
+    browser.close()
+
+def test_secondTest(page:Page):
+    page.goto('https://eventhub.rahulshettyacademy.com/login')
+    locator = page.get_by_placeholder('you@email.com')
+    locator.fill('beginner@sample.com')
+    assert(locator.input_value() == 'beginner@sample.com')    
+
+def test_thirdTest(playwright: Playwright):
+    browser = playwright.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://eventhub.rahulshettyacademy.com/login')
+    expect(page.get_by_role('heading',name='Sign in to EventHub')).to_be_visible()
+    locator = page.get_by_placeholder('you@email.com')
+    assert locator.input_value() == ''    #assert locator2.to.have.value()
+    context.close()
+    browser.close()  
+
+    
+
