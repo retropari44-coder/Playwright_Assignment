c57961c85c64b3a7bbed13db76cffc0e239a8986
retropari44-coder <retropari44@gmail.com>
2026-09-26 01:57:56 +0530

    Assignment 4 Updated

--- Assignment4/test_assignment4.py
+++ Assignment4/test_assignment4.py
@@ -37,12 +37,39 @@ class Verification:
         for index in range(booked_details_locator.count()):
             key = booked_details_locator.nth(index).locator('span').first.inner_text()
             values= booked_details_locator.nth(index).locator('span').last.inner_text()
-            print(f'{key}:{values}')
             details[key]=values
-        
+        Title = self.page.get_by_role('heading').first.inner_text()
+        details['Title'] = Title
         return details
 
+    def check_details(self, booking_details, test):
+            count = 0
+            for i in booking_details.keys():
+                for j in test:
+                    if booking_details[i] in j:
+                        count +=1
+                        break
+            assert count == (len(booking_details)-1)
 
+    # def get_booking_details(self,search_word):
+    #     self.page.get_by_role('button',name='View My Bookings').click()
+    #     event_card_locator = self.page.locator('#booking-card')
+    #     expect(event_card_locator.first).to_be_visible()
+    #     event_card_locator_count = event_card_locator.count()
+    #     for index in range(event_card_locator_count):
+    #         is_present = event_card_locator.nth(index).filter(has_text=search_word).is_visible()
+    #         if is_present:
+    #             event_card_locator.nth(index).filter(has_text=search_word).get_by_role('button',name ='View Details').click()
+    #             break
+    #     expect(self.page).to_have_url(re.compile(r"/\d+$"))
+    #     expect(self.page.locator('.space-y-4')).to_be_visible()
+    #     texts = self.page.locator('.space-y-4').locator('span').all_inner_texts()
+    #     booking_dictonary = {}
+    #     for i in range(0,len(texts)-1,2):
+    #         booking_dictonary[texts[i]] = texts[i+1]
+    #     return booking_dictonary
+
+            
     def book_tickets(self,no_of_tickets,name,email,phone):
         form_locator=self.page.locator(".space-y-4")
         for _ in range(no_of_tickets-1):
@@ -53,15 +80,89 @@ class Verification:
         form_locator.get_by_role('button',name='Confirm Booking').click()
         expect(self.page.get_by_text(re.compile(r'Booking Confirmed'))).to_be_visible()
 
+    def verify_first_test(self):
+        self.page.get_by_role('button',name='View My Bookings').click()
+        event_card_locator = self.page.locator('#booking-card')
+        expect(event_card_locator.first).to_be_visible()
+        expect(event_card_locator.first.get_by_role('heading',name='World Tech Summit')).to_be_visible()
+        expect(event_card_locator.first.locator('.booking-ref')).not_to_have_text('')
+        expect(event_card_locator.first).to_have_text(re.compile(r'1 ticket'))
+
+    def verify_second_test(self, booking_reference=''):
+        self.page.get_by_role('button',name='View My Bookings').click()
+        event_card_locator = self.page.locator('#booking-card')
+        expect(event_card_locator.first).to_be_visible()
+        expect(event_card_locator.first.locator('.booking-ref')).to_have_text(booking_reference)
+        expect(event_card_locator.first).to_have_text(re.compile(r'2 ticket'))
+        first_tile = event_card_locator.first.locator('h3').inner_text()
+        second_title = event_card_locator.last.locator('h3').inner_text()
+        assert (first_tile != second_title)
+
+    def verify_third_test(self, booking_reference):
+        event_card_locator = self.page.locator('#booking-card',has_text=booking_reference)
+        expect(event_card_locator).to_be_visible()
+        expect(event_card_locator.locator('span',has_text='confirmed')).to_be_visible()
+        event_card_locator = event_card_locator.inner_text()
+        split_lines = event_card_locator.split('\n')
+        return split_lines
+         
     
+
+    def verify_fourth_test(self, booking_reference1,booking_reference2):
+        event_card_locator = self.page.locator('#booking-card',has_text=booking_reference1['Booking Ref'])
+        expect(event_card_locator).to_be_visible()
+        booking_id = event_card_locator.locator('#booking-id').inner_text()
+        booking_id = booking_id[1:]
+        event_card_locator.get_by_role('button',name='View Details').click()
+    
+        expect(self.page).to_have_url(re.compile(booking_id))
+        expect(self.page.get_by_text(booking_reference1['Booking Ref']).first).to_be_visible()
+        expect(self.page.get_by_role('heading',name=booking_reference1['Title'])).to_be_visible()
+        expect(self.page.get_by_text(str(booking_reference1['Tickets']), exact=True)).to_be_visible()
+        expect(self.page.get_by_text(str(booking_reference1['Total'])).first).to_be_visible()
+        expect(self.page.get_by_text(booking_id)).to_be_visible()
+
+        self.page.get_by_role('button',name='My Bookings').click()
+        event_card_locator = self.page.locator('#booking-card',has_text=booking_reference2['Booking Ref'])
+        expect(event_card_locator).to_be_visible()
+        event_card_locator.get_by_role('button',name='View Details').click()
+        expect(self.page.get_by_text(booking_reference2['Booking Ref']).first).to_be_visible()
+        expect(self.page.get_by_role('heading',name = booking_reference2['Title']).first).to_be_visible()
+        expect(self.page.get_by_text(str(booking_reference2['Tickets']), exact=True)).to_be_visible()
+        expect(self.page.get_by_text(str(booking_reference2['Total'])).first).to_be_visible()
+        expect(self.page.get_by_text(str(booking_reference1['Total'])).first).not_to_be_visible()
+
+
+
 
 def test_login_verification(page: Page):
     login_page = Verification(page)
     login_page.login('loguraj568@gmail.com','March@0329')
+
     login_page.search_for_booking(search_word='World',city='Hyderabad')
     login_page.book_tickets(no_of_tickets=1,name='Prithvi',email='loguraj568@gmail.com',phone='7708196869')
+    booking_details1 = login_page.retrive_booking_details().copy()
+    print(booking_details1)
+    login_page.verify_first_test()
+    
+    # full_booking_details1 = login_page.get_booking_details(search_word='World')
+    # print(full_booking_details1)
+
     login_page.search_for_booking(search_word='Dilli',city='Delhi')
     login_page.book_tickets(no_of_tickets=2,name='Prithvi',email='loguraj568@gmail.com',phone='7708196869')
+    booking_details2 = login_page.retrive_booking_details().copy() #because we are using single object for all
+    print(booking_details2)
+
+    login_page.verify_second_test(booking_reference=booking_details2['Booking Ref'])
+    test1 = login_page.verify_third_test(booking_reference=booking_details1['Booking Ref']).copy()
+    test2 = login_page.verify_third_test(booking_reference=booking_details2['Booking Ref']).copy()
+    
+    login_page.check_details(booking_details1, test1)
+    login_page.check_details(booking_details2, test2)
+
+    login_page.verify_fourth_test(booking_reference1=booking_details1,booking_reference2=booking_details2)
+
+
 
 
     
